e450b8e608857aa00634e62bb0d338bd2c79aa61
cpaciencia <cpaciencia@hotmail.com>
2024-02-16 16:21:53 +0100

    add authors microservice

---
+++ src/main/java/com/tenantevaluation/training/movies/web/resources/ExceptionHandlerController.java
@@ -0,0 +1,19 @@
+package com.tenantevaluation.training.movies.web.resources;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class ExceptionHandlerController {
+
+    /*
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException() {
+        ErrorResponse errorResponse = ErrorResponse.builder().message("Internal Error")
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value()).build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }*/
+
+
+}
